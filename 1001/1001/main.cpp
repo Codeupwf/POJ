cb66b20b2204155580705fed0cac443f364c43ac
Codeup <codeup.wf@gmail.com>
2013-06-06 23:27:23 +0800

    1001

---
+++ 1001/1001/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  1001
+//
+//  Created by 王芾 on 13-6-6.
+//  Copyright (c) 2013年 王芾. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+    int a,b;
+    std::cin >> a >> b;
+    std::cout << a + b << std::endl;
+    return 0;
+}
+
---
+++ 1002/1002/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  1002
+//
+//  Created by 王芾 on 13-6-6.
+//  Copyright (c) 2013年 王芾. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
